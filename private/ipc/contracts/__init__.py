7054c4f32938a65d01a0c62a7b70519160c264e9
Amit Pandey <amit08072005@gmail.com>
2025-09-01 00:30:14 +0530

    added erc1967
    Signed-off-by: Amit Pandey <amit08072005@gmail.com>

--- private/ipc/contracts/__init__.py
+++ private/ipc/contracts/__init__.py
@@ -1,4 +1,12 @@
 from .storage import StorageContract
 from .access_manager import AccessManagerContract
+from .erc1967_proxy import ERC1967Proxy, ERC1967ProxyMetaData, new_erc1967_proxy, deploy_erc1967_proxy
 
-__all__ = ['StorageContract', 'AccessManagerContract']
+__all__ = [
+    'StorageContract', 
+    'AccessManagerContract',
+    'ERC1967Proxy',
+    'ERC1967ProxyMetaData',
+    'new_erc1967_proxy',
+    'deploy_erc1967_proxy'
+]
